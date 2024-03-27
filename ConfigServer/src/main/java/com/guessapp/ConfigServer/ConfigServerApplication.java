8bf696198c764185d4ce13ddb689c6fb8f4a739e
SemihArslanhw <semiharslanhw@gmail.com>
2024-03-27 06:19:22 +0300

    initial

---
+++ ConfigServer/src/main/java/com/guessapp/ConfigServer/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.guessapp.ConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
